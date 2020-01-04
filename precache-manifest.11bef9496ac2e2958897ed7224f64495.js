2f8cfbb2be8e07141e7fb63d9ad0d5cc888660b5
Abhigyan <abhigyanmahanta001@gmail.com>
2020-01-04 13:48:31 +0530

    Updates

---
+++ precache-manifest.11bef9496ac2e2958897ed7224f64495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b8beb3b81aafbe716037e994d33da8",
+    "url": "/monster_card/index.html"
+  },
+  {
+    "revision": "d0f537b503350b8bc616",
+    "url": "/monster_card/static/css/main.d599f085.chunk.css"
+  },
+  {
+    "revision": "2dec24adca8eb823ead4",
+    "url": "/monster_card/static/js/2.478aa170.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster_card/static/js/2.478aa170.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0f537b503350b8bc616",
+    "url": "/monster_card/static/js/main.fd08dbba.chunk.js"
+  },
+  {
+    "revision": "e5d2c8828114cf734a28",
+    "url": "/monster_card/static/js/runtime-main.124b2877.js"
+  }
+]);
